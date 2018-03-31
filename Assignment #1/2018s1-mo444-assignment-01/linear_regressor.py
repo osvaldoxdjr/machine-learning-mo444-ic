5c06a67820d8f00a3694aaa3e904ca873e407f7e
osvaldoxdjr <osvaldoxdjr@gmail.com>
2018-03-31 09:40:55 -0300

    Code organized

--- Assignment #1/2018s1-mo444-assignment-01/linear_regressor.py
+++ Assignment #1/2018s1-mo444-assignment-01/linear_regressor.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import random
 
 def perform_GD(thetas, LR, feature_train, target_train, reg=False, lamb=0):
@@ -28,10 +29,8 @@ def perform_NE(feature_train, target_train, reg, lamb):
 
 def cost_function(thetas, data_test, data_target, reg=False, lamb=0):
     m_examples = np.shape(data_test)[0]
-    if reg == True:
-        error = np.square((np.dot(thetas, data_test.transpose())) - data_target)
-    else:
-        error = np.square((np.dot(thetas, data_test.transpose())) - data_target)
+    error = np.square((np.dot(thetas, data_test.transpose())) - data_target)
+
     if reg == True:
         return (np.sum(error)+lamb*np.sum(np.square(thetas))) / (2 * m_examples)
     else:
@@ -46,11 +45,9 @@ def remove_discrete_variables(feature_train):
     return np.delete(feature_train, discrete_index, 1)
 
 def increase_complexity(feature_train, comp):
-
-    for c in range(comp):
-        feature_train = np.concatenate((feature_train,np.power(feature_train,c)),1)
-    print(feature_train)
-    i = input('teste')
+    aux = feature_train
+    for c in range(2,comp+1):
+        feature_train = np.concatenate((feature_train,np.power(aux,c)),1)
     return feature_train
 
 def plot_function_2d(y, x, ne):
@@ -86,10 +83,12 @@ def plot_function_3d(y, x, ne):
 
 if __name__ == "__main__":
 
-    LRs = [0.1, 0.001, 0.0001]
+    LRs = [0.1]
     reg = True
-    lamb = 1
+    rem_disc = True
+    lamb = 0.5
     comp = 1
+    norm = True
     x_final = []
     y_final = []
 
@@ -105,16 +104,12 @@ if __name__ == "__main__":
         target_train = data_train.iloc[:25000,60]
 
         # Select validation predictable features and target
-        feature_validation = data_train.iloc[24501:,2:60]
-        target_validation = data_train.iloc[24501:,60]
+        feature_validation = data_train.iloc[25001:,2:60]
+        target_validation = data_train.iloc[25001:,60]
 
         # Select test predictable features and target
         feature_test = data_test.iloc[:,2:]
         target_test = data_test_target
-
-        # Adjust index after iloc
-        feature_validation = feature_validation.reset_index(drop=True)
-        target_validation = target_validation.reset_index(drop=True)
 
         # Transforming into array
         feature_train = np.asarray(feature_train)
@@ -130,22 +125,26 @@ if __name__ == "__main__":
         te = np.ones(np.shape(feature_test)[0])
 
         # Feature Scalling
-        scale_sel = 'minmax'
-        if scale_sel == 'minmax':
-            X_scaler =  MinMaxScaler()
-        else:
-            X_scaler = StandardScaler()
-        feature_train  = X_scaler.fit_transform(feature_train)
-        feature_validation = X_scaler.transform(feature_validation)
-        feature_test = X_scaler.transform(feature_test)
+        if norm == True:
+            scale_sel = 'minmax'
+            if scale_sel == 'minmax':
+                X_scaler =  MinMaxScaler()
+            else:
+                X_scaler = StandardScaler()
+            feature_train  = X_scaler.fit_transform(feature_train)
+            feature_validation = X_scaler.fit_transform(feature_validation)
+            feature_test = X_scaler.transform(feature_test)
 
         # Remove discrete variables
-        #feature_train = remove_discrete_variables(feature_train)
-        #feature_validation = remove_discrete_variables(feature_validation)
-        #feature_test = remove_discrete_variables(feature_test)
+        if rem_disc == True:
+            feature_train = remove_discrete_variables(feature_train)
+            feature_validation = remove_discrete_variables(feature_validation)
+            feature_test = remove_discrete_variables(feature_test)
 
         # Increase Complexity
         feature_train = increase_complexity(feature_train, comp)
+        feature_validation = increase_complexity(feature_validation, comp)
+        feature_test = increase_complexity(feature_test, comp)
 
         # Inserting X0 to feature datasets
         np.insert(feature_train, 0, tr)
@@ -155,8 +154,10 @@ if __name__ == "__main__":
         # Plot hist
         plot_hist(target_train)
         m_before = np.shape(feature_train)[0]
-        print(np.shape(feature_train), np.shape(target_train))
-        feature_train, target_train = removing_examples(feature_train, target_train, 4000)
+
+        # Removing Examples
+        feature_train, target_train = removing_examples(feature_train, target_train, 5000)
+
         m_after = np.shape(feature_train)[0]
         print('%i examples were removed!'%(m_before-m_after))
         plot_hist(np.asarray(target_train))
@@ -172,33 +173,69 @@ if __name__ == "__main__":
         n = 1
 
         # Perfoming GD and calulating cost function
-        for i in range(1000):
+        for i in range(10000):
             n_value = cost_function(thetas, feature_train, target_train, reg, lamb)
             y.append(n_value)
             x.append(n)
             n += 1
             diff = n_value-a_value
             if i%200 == 0:
+                ''''
                 print("Performing GD!\n")
                 print("It has passed %i iterations"%i)
                 print("The cost is: %e" % n_value)
                 print("The diff from previous and actual is: %e\n"%diff)
+                '''
             pred = (np.dot(thetas, feature_train.transpose()))
             thetas = perform_GD(thetas, LR, feature_train, target_train, reg, lamb)
             a_value = n_value
 
         thetas_NE = perform_NE(feature_train, target_train, reg, lamb)
+
         cost_NE = cost_function(thetas_NE,feature_train,target_train, reg, lamb)
         cost_GD = cost_function(thetas, feature_train, target_train, reg, lamb)
+        print('Train\nCost NE: %e\nCost GD: %e\n'%(cost_NE, cost_GD))
+        cost_NE_v = cost_function(thetas_NE,feature_validation,target_validation, reg, lamb)
+        cost_GD_v = cost_function(thetas, feature_validation, target_validation, reg, lamb)
+        print('Validation\nCost NE: %e\nCost GD: %e\n'%(cost_NE_v, cost_GD_v))
 
-        print(thetas)
-        print(thetas_NE)
+        #print(thetas)
+        #print(thetas_NE)
 
-        print('Cost NE: %e\nCost GD: %e\n'%(cost_NE, cost_GD))
+        #plot_function_2d(y,x, np.full(len(x),cost_NE))
 
-        plot_function_2d(y,x, np.full(len(x),cost_NE))
+        y_final.append(y)
 
+        print('GD')
         mean_err = abs((target_train - np.dot(thetas, feature_train.transpose()))).mean()
         print("(Train) A média de erro do número de compartilhamentos é %.2f"%mean_err)
         mean_err = abs((target_validation - np.dot(thetas, feature_validation.transpose()))).mean()
         print("(Validation) A média de erro do número de compartilhamentos é %.2f"%mean_err)
+        print('NE')
+        mean_err = abs((target_train - np.dot(thetas_NE, feature_train.transpose()))).mean()
+        print("(Train) A média de erro do número de compartilhamentos é %.2f"%mean_err)
+        mean_err = abs((target_validation - np.dot(thetas_NE, feature_validation.transpose()))).mean()
+        print("(Validation) A média de erro do número de compartilhamentos é %.2f"%mean_err)
+
+    name_plot = 'reg-' + str(reg) + '_' + 'comp-' + str(comp) + '_' + 'lambda-' + str(lamb) + '_' + 'rem disc-' + str(rem_disc)
+
+    # Create plots with pre-defined labels.
+    fig, ax = plt.subplots()
+    ax.plot(x, y_final[0], 'k-', label='LR = 0.1')
+    ax.plot(x, y_final[1], 'k--', label='LR = 0.01')
+    ax.plot(x, y_final[2], 'k-.', label='LR = 0.001')
+    ax.plot(x, np.full(len(x),cost_NE), 'k:', label='Normal Equation')
+
+    legend = ax.legend(loc='upper center', shadow=True, fontsize='x-large')
+
+    plt.ylabel('Cost Function')
+    plt.xlabel('Number of Interactions')
+
+    ax.yaxis.get_major_formatter().set_powerlimits((0, 1))
+
+    # Put a nicer background color on the legend.
+    legend.get_frame().set_facecolor('#00FFCC')
+
+    plt.savefig(name_plot+'.png')
+
+    plt.show()
